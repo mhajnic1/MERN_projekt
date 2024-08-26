6a9755c24b71f0791c84652835eb37c9a9c4553e
Luka Antunovic <luka.antunovic6@gmail.com>
2024-08-26 20:06:41 +0200

    Setting up the backend

---
+++ server/routes/dalleRoutes.js
@@ -0,0 +1,42 @@
+import express from 'express';
+import * as dotenv from 'dotenv';
+import OpenAI from 'openai';
+import axios from 'axios';
+dotenv.config();
+
+const router = express.Router();
+
+
+
+const openai = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY,
+});
+
+router.route('/').get((req, res) => {
+    res.send('Diamond is unbreakable');
+});
+
+router.route('/').post(async (req, res) => {
+    try {
+        const { prompt } = req.body;
+
+        const aiResponse = await openai.images.generate({
+            model:"dall-e-3",
+            prompt,
+            size: '1024x1024',
+            quality:"standard",
+            n:1,
+        });
+        console.log(aiResponse);
+        
+
+        const image = aiResponse.data[0].url;
+        res.status(200).json({ photo: image });
+    } catch (error) {
+        console.error(error);
+        const errorMessage = error?.response?.data?.error?.message || "Something went wrong!";
+        res.status(500).send(errorMessage);
+    }
+});
+
+export default router;
