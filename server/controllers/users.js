0b508f36a4966137ea8ef9fa04a59d28da07dee1
Luka Antunovic <luka.antunovic6@gmail.com>
2024-09-05 12:08:24 +0200

    Fixed removing friends feature

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -38,12 +38,17 @@ export const addRemoveFriend = async (req, res) => {
     const friend = await User.findById(friendId);
 
     if (user.friends.includes(friendId)) {
-      user.friends = user.friends.filter((id) => id !== friendId);
-      friend.friends = friend.friends.filter((id) => id !== id);
+      user.friends = user.friends.filter((ID) => ID !== friendId);
+      friend.friends = friend.friends.filter((ID) => ID !== id);
     } else {
-      user.friends.push(friendId);
-      friend.friends.push(id);
+      if (!user.friends.includes(friendId)) {
+        user.friends.push(friendId);
+      }
+      if (!friend.friends.includes(id)) {
+        friend.friends.push(id);
+      }
     }
+
     await user.save();
     await friend.save();
 
@@ -58,6 +63,6 @@ export const addRemoveFriend = async (req, res) => {
 
     res.status(200).json(formattedFriends);
   } catch (err) {
-    res.status(404).json({ message: err.message });
+    res.status(500).json({ message: err.message });
   }
 };
